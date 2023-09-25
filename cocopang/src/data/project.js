a662c6e6ee78f1c3417e4de89b33ef4367f82741
c0c0pang <coc1789@naver.com>
2023-09-25 13:56:26 +0900

    feat: 프로젝트 데이터 추가

---
+++ cocopang/src/data/project.js
@@ -0,0 +1,30 @@
+import MeetMataSvg from '../../src/img/MeetMate.svg'
+import ResistanceSvg from '../../src/img/Resistance.svg'
+import MOVISvg from '../../src/img/MOVI.svg'
+import ZeroUNISvg from '../../src/img/ZeroUNI.svg'
+export const projectData = [
+    {
+        id: 0,
+        name: 'MeetMate',
+        state: '프로젝트',
+        svg: MeetMataSvg
+    },
+    {
+        id: 1,
+        name: 'Resistance Girls',
+        state: '프로젝트',
+        svg: ResistanceSvg
+    },
+    {
+        id: 2,
+        name: 'MOVI',
+        state: '프로젝트',
+        svg: MOVISvg
+    },
+    {
+        id: 3,
+        name: 'ZeroUNI',
+        state: '프로젝트-진행중',
+        svg: ZeroUNISvg
+    },
+]
