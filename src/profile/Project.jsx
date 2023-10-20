b688a013ce15ce845b6c8871aea94b79be0d4d6e
c0c0pang <coc1789@naver.com>
2023-10-21 02:30:48 +0900

    feat: 하나의 버튼만 눌러도 변경되었던 버그 수정

--- src/profile/Project.jsx
+++ src/profile/Project.jsx
@@ -1,26 +1,44 @@
-import React, { useState } from 'react'
-import { ProjectCategoryDiv, ProjectCategoryText, ProjectContainer, ProjectHeaderDiv, ProjectHeaderText, ProjectListDiv } from '../style/Project'
-import ProjectCard from './ProjectCard'
+import React, { useState } from 'react';
+import { ProjectCategoryDiv, ProjectCategoryText, ProjectContainer, ProjectHeaderDiv, ProjectHeaderText, ProjectListDiv } from '../style/Project';
+import ProjectCard from './ProjectCard';
 
 function Project() {
-    const [colorCheck, setColorCheck] = useState(true)
-    const onColorHandler = () => {
-        colorCheck ? setColorCheck(false) : setColorCheck(true)
-    }
+    const [colorCheck, setColorCheck] = useState(true);
+
+    const handleCategoryClick = (category) => {
+        if (colorCheck !== category) {
+            setColorCheck(category);
+        }
+    };
+
     return (
         <ProjectContainer>
             <ProjectHeaderDiv>
                 <ProjectHeaderText fontFamily='GmarketB' color='#fff'>Project</ProjectHeaderText>
             </ProjectHeaderDiv>
             <ProjectCategoryDiv gap={20} check={colorCheck}>
-                <ProjectCategoryText fontFamily='GmarketB' color='#fff' id='web' onClick={() => onColorHandler()}>Web</ProjectCategoryText>
-                <ProjectCategoryText fontFamily='GmarketB' color='#fff' id='app' onClick={() => onColorHandler()}>App</ProjectCategoryText>
+                <ProjectCategoryText
+                    fontFamily='GmarketB'
+                    color='#fff'
+                    id='web'
+                    onClick={() => handleCategoryClick(true)}
+                >
+                    Web
+                </ProjectCategoryText>
+                <ProjectCategoryText
+                    fontFamily='GmarketB'
+                    color='#fff'
+                    id='app'
+                    onClick={() => handleCategoryClick(false)}
+                >
+                    App
+                </ProjectCategoryText>
             </ProjectCategoryDiv>
             <ProjectListDiv cnumber={3}>
                 <ProjectCard colorCheck={colorCheck} />
             </ProjectListDiv>
         </ProjectContainer>
-    )
+    );
 }
 
-export default Project
+export default Project;
