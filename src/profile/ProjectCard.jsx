5938b86b2ad858e939de1dbf458a5e3ee42f5945
c0c0pang <coc1789@naver.com>
2023-10-21 02:16:13 +0900

    feat: 각 버튼에 맞는 카드를 보여주도록 구현

--- src/profile/ProjectCard.jsx
+++ src/profile/ProjectCard.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { projectData } from '../data/project'
-import { ProjectCardDiv, ProjectCardText, ProjectCardTextDiv, ProjectImg } from '../style/Project'
+import React from 'react';
+import { projectData } from '../data/project';
+import { ProjectCardDiv, ProjectCardText, ProjectCardTextDiv, ProjectImg } from '../style/Project';
 
-function ProjectCard() {
+function ProjectCard(props) {
+    const filteredProjects = projectData.filter((e) => {
+        if (props.colorCheck) {
+            return e.type === 'Web';
+        } else {
+            return e.type === 'App';
+        }
+    });
+
     return (
-        projectData.map((e) =>
-        (
-            <ProjectCardDiv id='test'>
-                <ProjectImg src={e.svg} />
-                <ProjectCardTextDiv>
-                    <ProjectCardText fontFamily='GmarketB' color='#fff'> {e.name}</ProjectCardText>
-                    <ProjectCardText fontFamily='GmarketB' color='#fff'> {e.state}</ProjectCardText>
-                </ProjectCardTextDiv>
-            </ProjectCardDiv>
-        )
-        )
-    )
+        <>
+            {filteredProjects.map((project, index) => (
+                <ProjectCardDiv id='test' key={index}>
+                    <ProjectImg src={project.svg} />
+                    <ProjectCardTextDiv>
+                        <ProjectCardText fontFamily='GmarketB' color='#fff'>{project.name}</ProjectCardText>
+                        <ProjectCardText fontFamily='GmarketB' color='#fff'>{project.state}</ProjectCardText>
+                    </ProjectCardTextDiv>
+                </ProjectCardDiv>
+            ))}
+        </>
+    );
 }
 
-export default ProjectCard
+export default ProjectCard;
