31529834e833baa09e284a16bd9258c4e7ff8c21
c0c0pang <coc1789@naver.com>
2023-10-21 02:14:57 +0900

    feat: Web과 App을 구별하는 버튼 추가

--- src/profile/Project.jsx
+++ src/profile/Project.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { ProjectContainer, ProjectHeaderDiv, ProjectHeaderText, ProjectListDiv } from '../style/Project'
+import React, { useState } from 'react'
+import { ProjectCategoryDiv, ProjectCategoryText, ProjectContainer, ProjectHeaderDiv, ProjectHeaderText, ProjectListDiv } from '../style/Project'
 import ProjectCard from './ProjectCard'
 
 function Project() {
+    const [colorCheck, setColorCheck] = useState(true)
+    const onColorHandler = () => {
+        colorCheck ? setColorCheck(false) : setColorCheck(true)
+    }
     return (
         <ProjectContainer>
             <ProjectHeaderDiv>
                 <ProjectHeaderText fontFamily='GmarketB' color='#fff'>Project</ProjectHeaderText>
             </ProjectHeaderDiv>
+            <ProjectCategoryDiv gap={20} check={colorCheck}>
+                <ProjectCategoryText fontFamily='GmarketB' color='#fff' id='web' onClick={() => onColorHandler()}>Web</ProjectCategoryText>
+                <ProjectCategoryText fontFamily='GmarketB' color='#fff' id='app' onClick={() => onColorHandler()}>App</ProjectCategoryText>
+            </ProjectCategoryDiv>
             <ProjectListDiv cnumber={3}>
-                <ProjectCard />
+                <ProjectCard colorCheck={colorCheck} />
             </ProjectListDiv>
         </ProjectContainer>
     )
